b46e1787496665b5906180725a23ccc17696d8eb
风灵 <116074189+jiageilvwunv@users.noreply.github.com>
2023-07-03 23:26:44 +0800

    Add files via upload

---
+++ 002.初识数据类型.c
@@ -0,0 +1,20 @@
+/*
+数据类型
+小数 - 浮点数（可以浮动15.6 = 1.56 * 10 ^ 1）
+整数-整型
+char		//字符数据类型（a，b，c）
+short		//短整型
+int			//整型
+long		//长整型
+long long	//更长的整形
+float		//单精度浮点数
+double		//双精度浮点数
+sizeof-计算类型或者变量所占空间的大小
+printf("%d\n",sizeof(char))
+*/
+/*
+计算机中的单位
+bit-比特位（计算机中识别二进制一个二进制位）
+byte-字节=8bit		kb=1024byte			mb=1024kb
+gb=1024mb				tb=1024gb			pb=1024tb
+*/
